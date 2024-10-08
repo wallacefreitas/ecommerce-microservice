57de493be15e0c6d0327441979d553a9fee6fd6c
Wallace Freitas <wallace.freitas.b@gmail.com>
2024-10-07 21:13:50 -0300

    chore: added migration of create stocks and prodcuts tables

---
+++ services/product/prisma/migrations/20241006131758_create_table_products/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL NOT NULL,
+    "image" TEXT
+);
