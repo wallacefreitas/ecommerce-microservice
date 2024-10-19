c154d21fbad3dd3c2a6d988564ff473d45e44e7b
Wallace Freitas <wallace.freitas.b@gmail.com>
2024-10-19 11:54:37 -0300

    chore: adjust folders to adapt clean architecture principles

--- services/inventory/src/infra/services/kafka/events.ts
+++ services/inventory/src/infra/services/kafka/events.ts
@@ -1,5 +1,5 @@
 import { consumeProductCreatedEvent } from './consumers/product-consumer';
-import { KafkaService } from '@infra/services/kafka';
+import { KafkaService } from '@infra/services/kafka/kafka';
 
 export async function startEventConsumers() {
   const kafkaService = new KafkaService();
